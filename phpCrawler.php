262e08442d07bf5d9a8eaba58a16068ccc57b669
Yao Wang <yao422321065@yahoo.com>
2017-02-14 17:01:59 -0500

    crawled all post on cnusa

--- phpCrawler.php
+++ phpCrawler.php
@@ -1,21 +1,74 @@
 <?php
+    ini_set('max_execution_time',3000);//5 mins execution time
+    
     include_once "simple_html_dom.php";
     echo "<html><head><title>PHP Crawler</title></head><body>";
     echo "<p>PHP Crawler</p>";
+      
     
     $url='http://cnusa.org/readnews.aspx?newsid=';
-    $post_id = '14730';
-    $url = $url.$post_id;
+    //$post_id = '1000';
+    //$url = $url.$post_id;
     $host = 'http://cnusa.org';
+    
+/*checked #
+*1-6990                 15
+*6990-13500      339
+*13500-14300    49
+*14300-14500    23
+*14500-14730    212
+*/
+    $start_id='1';
+    //$end_id='14730';
+    $end_id='6990';
+    $crawled_num=0;
+    
+    ob_implicit_flush(true);
+    ob_start();
+    echo "Start executing!";
+    ob_flush();//display it simultaneously
+while($start_id<$end_id){
+    $url = 'http://cnusa.org/readnews.aspx?newsid='.$start_id;
+    $post_id = $start_id;
+    
+        $raw_result = curl_get($url);//get html file
+        if($raw_result[0]!==200){//detect if post exist
+            //echo "already dead".$post_id;
+            //die("Unexpected HTTP code: ".$raw_result[0]."\n");//post doesn't exist
+            echo "<span>skip post id ".$post_id."  </span>";
+            ob_flush();
+            $start_id++;
+            continue;
+        }
+        $result = $raw_result[1];//post exist
+       try{ 
+           manipulate_post($url,$host,$post_id,$result); 
+          }catch(Exception $e){
+        echo 'Caught exception: ',  $e->getMessage(), "\n";
+        die();
+         }
+        echo "<span style='color:#00FF00'>crawled ".$post_id."  </span>";
+        ob_flush();
+        $start_id++;
+        $crawled_num++;
+        //sleep(1);
+   
+    
+}
+    echo "done!";
+    echo "<span style='color:#00FF00'> ".$crawled_num." posts crawled!</span>";
+    ob_end_flush();
 
-    $raw_result = curl_get($url);
-    if($raw_result[0]!==200){
-        die("Unexpected HTTP code: ".$raw_result[0]."\n");
-        echo "already dead";
-    }
-    $result = $raw_result[1];
+
+
+    //print_r(search_db($post_id));
+
+    echo "</body></html>";
+//end HTML
+
+function manipulate_post($url,$host,$post_id,$result){
+    
     //$result = preg_replace("/\s/","",$result);//get rid of all kinds of whiteSpace
-  
 /** parse HTML -- regex solution 
     $pattern = '/<span class="rank">(.*?)<\/span>.*?<div class="score unvoted" [^>]*>(.*?)<\/div>.*?<p class="title"><a[^>]*href="(.*?)"[^>]*>(.*?)<\/a>/';
     
@@ -32,8 +85,7 @@
     $html = str_get_html($result);//create dom object
     $post_title = $html->find('h3[class=entry-title]',0);
     $post_title = $post_title->plaintext;//get post title
-
-
+//start init variables
     $post_date_day = $html->find('.date .day',0);
     $post_date_day = trim($post_date_day->plaintext);//get post day
     $post_date_month = $html->find('.date .month',0);
@@ -45,37 +97,31 @@
     //$post_body_plaintext = $post_body->plaintext;
     
     $post_body_segments = $post_body->find('p');//multiple result
-    
-/*
-save tile,data,body imgs
-body to folder posts
-imgs to imgs 
-name folder as dates (if exist add # at the end)
-replace the images in postbody with the address
-*/
 
-    echo "post id is: ".$post_id."<br>";
-    echo "post title is: ".$post_title."<br>";
-    echo "post was published on Year: ".$post_date_year." Month: ".$post_date_month." Day: ".$post_date_day."<br>";
-    //echo "post body(plaintext)"."<br><br>";
-    //echo $post_body_plaintext;
+    //display current manipulating post
+    //echo "post id is: ".$post_id."<br>";
+    //echo "post title is: ".$post_title."<br>";
+    //echo "post was published on Year: ".$post_date_year." Month: ".$post_date_month." Day: ".$post_date_day."<br>";
+    
     $post_imgs = array(0=>'',1=>'',2=>'',3=>'',4=>'',5=>'',6=>'',7=>'',8=>'');//post imgs local url
     $num = 1;//count the p's order
+    $num_for_pic = 1;//count the img's order
     $whole_content =$post_title.'\n\n' ;
     foreach ($post_body_segments as $item){
         
         if(has_pics($item)){
-            //echo "#".$num." is:"."there is a pic "."<br>";
+            
             $pic_url = $item->find("img",0)->getAttribute("src");
             $whole_content .=$pic_url."\n\n";
-            echo "#".$num." has a pic as ".$pic_url."<br>";
+            //echo "#".$num." has a pic as ".$pic_url."<br>";//display post's image local url
             $image_dir="images/".$post_date_year."/".$post_date_month."/".$post_date_day."/".$post_id."/".$num.".jpg";
             $image_url=$host.$pic_url;
             image_save($image_url,$image_dir);
-            $post_imgs[$num] = $image_dir;
+            $post_imgs[$num_for_pic] = $image_dir;
+            $num_for_pic++;
         }else{
             $item_plaintext = $item->plaintext;
-            echo "#".$num." is:".$item_plaintext."<br>";
+            //echo "#".$num." is:".$item_plaintext."<br>"; //display post body segement
             $whole_content .=$item_plaintext."\n\n";
         }
         $num++;
@@ -83,10 +129,11 @@ replace the images in postbody with the address
     $file_dir = $post_date_year."/".$post_date_month."/".$post_date_day."/".$post_id.".txt";
     file_save($file_dir,$whole_content);
    
-    create_database_sqlite();
+    create_database_sqlite();//init database
     
     $item=array();
-    $item["post_title"] = $post_title;
+    $item["post_id"] = $post_id;
+    $item["post_title"] = remove_CN_punctuation($post_title);
     $item["post_year"]=$post_date_year;
     $item["post_month"]=$post_date_month;
     $item["post_day"]=$post_date_day;
@@ -100,10 +147,16 @@ replace the images in postbody with the address
     $item["post_img7_url"]=$post_imgs[7];
     $item["post_img8_url"]=$post_imgs[8];  
 
-    add_to_db($item);
+    add_to_db($item);//insert into database, if $post_id duplicate, skip it
+}
 
-    echo "</body></html>";
-//end HTML
+
+function remove_CN_punctuation($keyword){
+    $keyword = urlencode($keyword);
+    $keyword=preg_replace("/(%7E|%60|%21|%40|%23|%24|%25|%5E|%26|%27|%2A|%28|%29|%2B|%7C|%5C|%3D|\-|_|%5B|%5D|%7D|%7B|%3B|%22|%3A|%3F|%3E|%3C|%2C|\.|%2F|%A3%BF|%A1%B7|%A1%B6|%A1%A2|%A1%A3|%A3%AC|%7D|%A1%B0|%A3%BA|%A3%BB|%A1%AE|%A1%AF|%A1%B1|%A3%FC|%A3%BD|%A1%AA|%A3%A9|%A3%A8|%A1%AD|%A3%A4|%A1%A4|%A3%A1|%E3%80%82|%EF%BC%81|%EF%BC%8C|%EF%BC%9B|%EF%BC%9F|%EF%BC%9A|%E3%80%81|%E2%80%A6%E2%80%A6|%E2%80%9D|%E2%80%9C|%E2%80%98|%E2%80%99)+/",' ',$keyword);
+    $keyword=urldecode($keyword);
+    return $keyword;
+}
 
 function file_save($dir,$contents){
     $parts = explode('/',$dir);
@@ -226,7 +279,8 @@ function debug_to_console($data){
 function create_database_sqlite(){
     $db = new SQLite3('cuscc') or die('Unable to open database');
     $query ='CREATE TABLE IF NOT EXISTS posts(
-        post_id INTEGER PRIMARY KEY,
+        id INTEGER PRIMARY KEY,
+        post_id INT(10) UNIQUE NOT NULL,
         post_title CHAR(100) NOT NULL,
         post_year  INT(4),
         post_month INT(2),
@@ -243,38 +297,34 @@ function create_database_sqlite(){
     )';
     $db->exec($query) or die('Create db failed');
 }
-function add_to_db($item=array("post_title"=>"",
-                               "post_year"=>"2005",
-                               "post_month"=>"1",
-                               "post_day"=>"1",
-                               "post_body"=>"",
-                               "post_img1_url"=>"",
-                               "post_img2_url"=>"",
-                               "post_img3_url"=>"",
-                               "post_img4_url"=>"",
-                               "post_img5_url"=>"",
-                               "post_img6_url"=>"",
-                               "post_img7_url"=>"",
-                               "post_img8_url"=>"")){
+function add_to_db($item=array("post_title"=>"", "post_year"=>"2005","post_month"=>"1","post_day"=>"1","post_body_url"=>"","post_img1_url"=>"","post_img2_url"=>"","post_img3_url"=>"","post_img4_url"=>"","post_img5_url"=>"","post_img6_url"=>"","post_img7_url"=>"","post_img8_url"=>"")){
     $db = new SQLite3('cuscc') or die('Unable to open database');
-    $query = 'INSERT INTO posts(post_title,post_year,post_month,post_day,post_body,post_img1_url,post_img2_url,post_img3_url,post_img4_url,post_img5_url,post_img6_url,post_img7_url,post_img8_url)
+    $query = 'INSERT OR IGNORE INTO posts(post_id,post_title,post_year,post_month,post_day,post_body_url,post_img1_url,post_img2_url,post_img3_url,post_img4_url,post_img5_url,post_img6_url,post_img7_url,post_img8_url)
     VALUES(
-        '.$item["post_title"].',
-        '.$item["post_year"].',
-        '.$item["post_month"].',
-        '.$item["post_day"].',
-        '.$item["post_body"].',
-        '.$item["post_img1_url"].',
-        '.$item["post_img2_url"].',
-        '.$item["post_img3_url"].',
-        '.$item["post_img4_url"].',
-        '.$item["post_img5_url"].',
-        '.$item["post_img6_url"].',
-        '.$item["post_img7_url"].',
-        '.$item["post_img8_url"].'
+        "'.$item["post_id"].'",
+        "'.$item["post_title"].'",
+        "'.$item["post_year"].'",
+        "'.$item["post_month"].'",
+        "'.$item["post_day"].'",
+        "'.$item["post_body"].'",
+        "'.$item["post_img1_url"].'",
+        "'.$item["post_img2_url"].'",
+        "'.$item["post_img3_url"].'",
+       "'.$item["post_img4_url"].'",
+        "'.$item["post_img5_url"].'",
+        "'.$item["post_img6_url"].'",
+        "'.$item["post_img7_url"].'",
+        "'.$item["post_img8_url"].'"
     )';
     $db->query($query) or die('add data failed'.$query);
     
+}
+
+function search_db($q){//$q==post_id
+    $db = new SQLite3('cuscc') or die('Unable to open database');
+    $query = 'SELECT * FROM posts WHERE post_id = "'.$q.'"';
+    $result = $db->query($query) or die('failed to search data');
+    return $result->fetchArray();
 }
 
 ?>
